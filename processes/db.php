279b643ddc85da67dec55b7eeec1a23e784fdb2b
Adeodatus Ludan <ludanadeodatus@aol.com>
2023-03-05 23:26:21 +0300

    Master

---
+++ processes/db.php
@@ -0,0 +1,7 @@
+<?php
+define("HOST", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DATABASE", "git_collab");
+
+$dbConnect = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
