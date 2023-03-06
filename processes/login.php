8bf4ca276da16e0f853905c7a676e32e2006f15d
Adeodatus Ludan <ludanadeodatus@aol.com>
2023-03-06 17:05:05 +0300

    some vital changes in the login file

--- processes/login.php
+++ processes/login.php
@@ -2,31 +2,29 @@
 require_once("./db.php");
 //initiation of script to handle login activities
 
-function handleLogin($email, $password){
-//Select relevant credential from database
-$credential = "SELECT email, password from tablename WHERE email = '$email'";
-//to run sql query direct to our database
-$query = mysqli_query($dbConnect, $credential);
-//to check if a runed query do not return null and fetch its information
-if($query){
-  $data = mysqli_fetch_array($query);
-//check if runed query returned with information or not
-if(sizeof($data!=0)){
-//if its size is not zero it means has data
-//lets us compaire a given data with user password
-$pass = $data['password'];
-if(password_verify($password,$pass)){
-    echo "login succes full";
-}
-else{
-    echo "sorry you enter wrong credential repeate again please";
-}
-}
-else{
-    echo "sorry you enter wrong credential repeate again please";
-}
-}
-else{
-    echo "Some thing wrong when run your sql query";
-}
+function handleLogin($dbConnect, $email, $password)
+{
+    //Select relevant credential from database
+    $credential = "SELECT email, password from tablename WHERE email = '$email'";
+    //to run sql query direct to our database
+    $query = mysqli_query($dbConnect, $credential);
+    //to check if a runed query do not return null and fetch its information
+    if ($query) {
+        $data = mysqli_fetch_array($query);
+        //check if runed query returned with information or not
+        if (sizeof($data) != 0) {
+            //if its size is not zero it means has data
+            //lets us compaire a given data with user password
+            $pass = $data['password'];
+            if (password_verify($password, $pass)) {
+                echo "login succes full";
+            } else {
+                echo "sorry you enter wrong credential repeate again please";
+            }
+        } else {
+            echo "sorry you enter wrong credential repeate again please";
+        }
+    } else {
+        echo "Some thing wrong when run your sql query";
+    }
 }
