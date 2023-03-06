30d0cb7c1815e5051e1da9e2a3222c9011c81b05
Adeodatus Ludan <ludanadeodatus@aol.com>
2023-03-07 00:07:19 +0300

    sorry nime arrange login

--- processes/login.php
+++ processes/login.php
@@ -3,56 +3,47 @@ require_once("./db.php");
 //initiation of script to handle login activities
 
 function handleLogin($email, $password)
-{ 
- global $dbConnect;
+{
+    global $dbConnect;
 
- if($email || $password){
- //Select relevant credential from database
-$credential = "SELECT user_name, password from users WHERE user_name = '$email'";
+    if ($email || $password) {
+        //Select relevant credential from database
+        $credential = "SELECT user_name, password from users WHERE user_name = '$email'";
 
-//to run sql query direct to our database
-$query = mysqli_query($dbConnect, $credential);
-
-
-//to check if a runed query do not return null and fetch its information
-if ($query) {
-$data = mysqli_fetch_array($query);
-//check if runed query returned with information or not
+        //to run sql query direct to our database
+        $query = mysqli_query($dbConnect, $credential);
 
 
-if ($data) {
-//if  fetch return true it means has data
-//lets us compaire a given data with user password
+        //to check if a runed query do not return null and fetch its information
+        if ($query) {
+            $data = mysqli_fetch_array($query);
+            //check if runed query returned with information or not
 
 
-$pass = $data['password'];
-if (password_verify($password, $pass)) {
-//direct user to its account
-header("location:../pages/user_account.html");
-} 
-else {
-$msg =  "sorry you enter wrong credential repeate again please";
-header("location:../pages/login.html?msg=$msg");
-}
+            if ($data) {
+                //if  fetch return true it means has data
+                //lets us compaire a given data with user password
 
 
-}
- else {
-$msg =  "sorry you enter wrong credential repeate again please";
-header("location:../pages/login.html?msg=$msg");
-}
-} else {
-echo "Some thing wrong when run your sql query";
-}
-
- }
- else{
-$msg =  "please make sure all filed is not empty";
-header("location:../pages/login.html?msg=$msg");
- }
-
+                $pass = $data['password'];
+                if (password_verify($password, $pass)) {
+                    //direct user to its account
+                    header("location:../pages/user_account.html");
+                } else {
+                    $msg =  "sorry you enter wrong credential repeate again please";
+                    header("location:../pages/login.html?msg=$msg");
+                }
+            } else {
+                $msg =  "sorry you enter wrong credential repeate again please";
+                header("location:../pages/login.html?msg=$msg");
+            }
+        } else {
+            echo "Some thing wrong when run your sql query";
+        }
+    } else {
+        $msg =  "please make sure all filed is not empty";
+        header("location:../pages/login.html?msg=$msg");
+    }
 }
 
 handleLogin($_POST['username'], $_POST['password']);
-
-
