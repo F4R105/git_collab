e4f4bc66d19b8b0957559b7c0867a4657abcb4a2
Adeodatus Ludan <ludanadeodatus@aol.com>
2023-03-06 22:21:27 +0300

    more changes

--- processes/register.php
+++ processes/register.php
@@ -7,13 +7,15 @@ function registration($dbConnect, $user_name, $password, $c_password)
 {
     if ($dbConnect && $user_name && $password && $c_password) {
         if ($password == $c_password) {
-            $select_query = "SELECT COUNT(users.user_name) as 'users_count' FROM users WHERE users.user_name = $user_name";
+            $select_query = "SELECT COUNT(users.user_name) as 'users_count' FROM users WHERE users.user_name = '$user_name'";
             $select_query_execution = mysqli_query($dbConnect, $select_query);
 
             if ($select_query_execution) {
                 $possible_users_count = mysqli_fetch_array($select_query_execution);
                 if ($possible_users_count['users_count'] == 0) {
-                    $insert_query = "INSERT INTO users (user_name, password) VALUES ($user_name, $c_password)";
+                    $c_password = password_hash($c_password, PASSWORD_DEFAULT);
+
+                    $insert_query = "INSERT INTO users (user_name, password) VALUES ('$user_name', '$c_password')";
                     $insert_query_execution = mysqli_query($dbConnect, $insert_query);
                     if ($insert_query_execution) {
                         $note = "Account created succesfully, log in to continue!";
