06441475fd647746ef1c2aea6b829659c4815dea
Hamisi Faraji Hamisi <hamisifaraji369@gmail.com>
2023-03-06 14:56:17 -0400

    addition in login functionality include ability of authenticate user, and exception handle

--- processes/login.php
+++ processes/login.php
@@ -2,29 +2,57 @@
 require_once("./db.php");
 //initiation of script to handle login activities
 
-function handleLogin($dbConnect, $email, $password)
-{
-    //Select relevant credential from database
-    $credential = "SELECT email, password from tablename WHERE email = '$email'";
-    //to run sql query direct to our database
-    $query = mysqli_query($dbConnect, $credential);
-    //to check if a runed query do not return null and fetch its information
-    if ($query) {
-        $data = mysqli_fetch_array($query);
-        //check if runed query returned with information or not
-        if (sizeof($data) != 0) {
-            //if its size is not zero it means has data
-            //lets us compaire a given data with user password
-            $pass = $data['password'];
-            if (password_verify($password, $pass)) {
-                echo "login succes full";
-            } else {
-                echo "sorry you enter wrong credential repeate again please";
-            }
-        } else {
-            echo "sorry you enter wrong credential repeate again please";
-        }
-    } else {
-        echo "Some thing wrong when run your sql query";
-    }
+function handleLogin($email, $password)
+{ 
+ global $dbConnect;
+
+ if($email || $password){
+ //Select relevant credential from database
+$credential = "SELECT user_name, password from users WHERE user_name = '$email'";
+
+//to run sql query direct to our database
+$query = mysqli_query($dbConnect, $credential);
+
+
+//to check if a runed query do not return null and fetch its information
+if ($query) {
+$data = mysqli_fetch_array($query);
+//check if runed query returned with information or not
+
+
+if (sizeof($data) != 0) {
+//if its size is not zero it means has data
+//lets us compaire a given data with user password
+
+
+$pass = $data['password'];
+if ($password == $pass) {
+//direct user to its account
+header("location:../pages/user_account.html");
+} 
+else {
+$msg =  "sorry you enter wrong credential repeate again please";
+header("location:../pages/login.html?msg=$msg");
 }
+
+
+}
+ else {
+$msg =  "sorry you enter wrong credential repeate again please";
+header("location:../pages/login.html?msg=$msg");
+}
+} else {
+echo "Some thing wrong when run your sql query";
+}
+
+ }
+ else{
+$msg =  "please make sure all filed is not empty";
+header("location:../pages/login.html?msg=$msg");
+ }
+
+}
+
+handleLogin($_POST['username'], $_POST['password']);
+
+
