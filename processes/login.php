01399a8145afd863b70d8778a297c8fa4f3f9356
cyxteen <49212444+Cyxteen@users.noreply.github.com>
2023-03-07 10:47:08 +0300

    changes the variable email to username in the login file

--- processes/login.php
+++ processes/login.php
@@ -2,13 +2,13 @@
 require_once("./db.php");
 //initiation of script to handle login activities
 
-function handleLogin($email, $password)
+function handleLogin($username, $password)
 {
     global $dbConnect;
 
-    if ($email || $password) {
+    if ($username || $password) {
         //Select relevant credential from database
-        $credential = "SELECT user_name, password from users WHERE user_name = '$email'";
+        $credential = "SELECT user_name, password from users WHERE user_name = '$username'";
 
         //to run sql query direct to our database
         $query = mysqli_query($dbConnect, $credential);
