a3ea191d358aaae03d0ac7c644f313c9a9addf08
cyxteen <49212444+Cyxteen@users.noreply.github.com>
2023-03-09 16:00:00 +0300

    minor changes to the user_list file

--- processes/user_list.php
+++ processes/user_list.php
@@ -3,7 +3,7 @@ require_once("../processes/db.php");
 
 function get_all_users($dbConnect)
 {
-    $select_query = "SELECT * FROM users";
+    $select_query = "SELECT user_name FROM users";
     $select_query_execution = mysqli_query($dbConnect, $select_query);
 
     $users = mysqli_fetch_array($select_query_execution);
