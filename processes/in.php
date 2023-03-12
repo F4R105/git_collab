266a9597398d58b1c2e3aa295e909a067c922206
Adeodatus Ludan <ludanadeodatus@aol.com>
2023-03-12 11:10:41 +0300

    Some minor changes in in.php file :)

--- processes/in.php
+++ processes/in.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
-if (empty($_SESSION['username'])) {
+$uri_fragments = explode("/", $_SERVER["REQUEST_URI"]);
+$current_page = end($uri_fragments);
+
+if (empty($_SESSION["username"]) && $current_page != "login.php") {
     header("location:./login.php");
+    die();
+} elseif (!empty($_SESSION["username"]) && $current_page == "login.php") {
+    header("location: ../pages/user_account.php");
     die();
 }
