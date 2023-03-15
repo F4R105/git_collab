f1169c450c9b962514f9cfcbd948d26b613403f2
cyxteen <49212444+Cyxteen@users.noreply.github.com>
2023-03-15 14:56:38 +0300

    ,minor changes to handle_likes and following file

--- processes/handle_following.php
+++ processes/handle_following.php
@@ -12,18 +12,12 @@ $user_id = $_SESSION['user_id'];
 insert_following($user_id, $author_id, $dbConnect);
 function insert_following($user_id, $author_id, $dbConnect){
     // checks if the author_id and the user_id are the same
-    if ($user_id == $author_id) {
-        $msg = base64_encode("you can't follow your own account!");
-        header("location: ../pages/blogs.php?msg=$msg");
-    }else {
-        $create_query = "INSERT INTO `following`(`user_id`, `author_id`) VALUES ($user_id,$author_id)";
-        $create_query_execution = mysqli_query($dbConnect, $create_query);
+    $create_query = "INSERT INTO `following`(`user_id`, `author_id`) VALUES ($user_id,$author_id)";
+    $create_query_execution = mysqli_query($dbConnect, $create_query);
 
-        if ($create_query_execution) {
-            return "True";
-        }else {
-            return "False";
-        }
-        
+    if ($create_query_execution) {
+        return "True";
+    }else {
+        return "False";
     }
 }
